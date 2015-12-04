cb92f6671ef4ed662d40c896be8c74f6d4ffbef4
Andrew Thompson <andrew@tobarja.com>
2015-12-03 23:25:47 -0500

    Initial Send Link by Email code

--- provision/config.php
+++ provision/config.php
@@ -4,4 +4,8 @@ define("DB_USER",'root');
 define("DB_PASS",'vagrant');
 define("DB_NAME",'sendatruck');
 define("TRUCK_REQUEST_URL", '/sendatruck');
+define("SMTP_HOSTNAME", 'localhost');
+define("SMTP_PORT", 25);
+define("SMTP_USERNAME", 'user');
+define("SMTP_PASSWORD", 'password');
 date_default_timezone_set('America/New_York');
---
+++ src/Contexts/EmailRequestLink.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SendATruck\Contexts;
+
+class EmailRequestLink
+{
+    /**
+     * @var \SendATruck\Services\EmailService
+     */
+    private $emailService;
+    private $requestUrl;
+    /**
+     * @var \SendATruck\Objects\Customer
+     */
+    private $customer;
+
+    public function __construct($emailService, $requestUrl, $customer)
+    {
+        $this->emailService = $emailService;
+        $this->requestUrl = $requestUrl;
+        $this->customer = $customer;
+    }
+
+    public function send()
+    {
+
+        $requestLink = BASEURL.$this->requestUrl.'/'.$this->customer->getId();
+        $message = "{$this->customer->getFirstName()},
+
+Use this link to request a pickup:
+
+{$requestLink}
+";
+        $this->emailService->mail(array(SMTP_FROM), array($this->customer->getEmail()), 'Your Request Link', $message);
+    }
+}
